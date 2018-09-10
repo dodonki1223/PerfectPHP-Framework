f4e161de5f2b6f41225509140c7144fbb9ec67b2
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2018-09-11 01:07:50 +0900

    getBaseUrl、getPathInfoメソッドのコメント文を追加

--- application/core/Request.php
+++ application/core/Request.php
@@ -17,7 +17,9 @@ class Request
     {
 
         if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
             return true;
+
         }
 
         return false;
@@ -89,9 +91,11 @@ class Request
     }
 
     /**
-     * リクエストされたURLを取得する
-     *   リクエストされたURLからホスト部分以降の値を返す
-     *   例：「http://hostname.com/sample/gorira」 → 「/sample/gorira」
+     * リクエストURLを取得する
+     *   リクエストされたURLからホスト部分以降の値を返す（REQUEST_URL）
+     *   例：「http://hostname.com/foo/bar/list(index.phpは"/foo/bar"以下) → 「/foo/bar」
+     *       「http://hostname.com/index.php/list?foo=bar」                → 「/index.php/list?foo=bar」
+     *       「http://hostname.com/」                                      → 「/」
      * 
      * @return string 
      */
@@ -102,16 +106,37 @@ class Request
 
     }
 
+    /**
+     * ベースURLを取得する
+     *   リクエストされたURLからベースURLを取得する
+     *   リクエストされたURLからホスト部分より後ろから、フロントコントローラまでの値を
+     *   $_SERVER[SCRIPT_NAME]、$_SERVER[REQUEST_URI]から取得し返す
+     *   ※ベースURLとは？（このフレームワークでのみの呼び方です）
+     *     ホスト部分より後ろから、フロントコントローラまでの値がベースURLになる
+     *     例：「http://hostname.com/foo/bar/list(index.phpは"/foo/bar"以下) → 「/foo/bar」
+     *         「http://hostname.com/index.php/list?foo=bar」                → 「/index.php」
+     *         「http://hostname.com/」                                      → 「」
+     *   ※SCRIPT_NAMEを使用した時に取得される値
+     *     例：「http://hostname.com/foo/bar/list(index.phpは"/foo/bar"以下) → 「/foo/bar/index.php」
+     *         「http://hostname.com/index.php/list?foo=bar」                → 「/index.php」
+     *         「http://hostname.com/」                                      → 「/index.php」
+     * 
+     * @return string 
+     */
     public function getBaseUrl()
     {
 
+        // スクリプト名とリクエストURLを取得する
         $scriptName = $_SERVER['SCRIPT_NAME'];
         $requestUri = $this->getRequestUri();
 
+        // フロントコントローラがURLに含まれている時
         if ( 0 === strpos($requestUri, $scriptName) ) {
 
             return $scriptName;
 
+        // フロントコントローラーが省略されている時（index.phpが省略）
+        // 例：http://example.com/foo/bar/list（index.phpは"foo/bar以下"）
         } else if ( 0 === strpos($requestUri, dirname($scriptName)) ) {
 
             return rtrim(dirname($scriptName), '/');
@@ -122,18 +147,35 @@ class Request
 
     }
 
+    /**
+     * PahtInfoを取得する
+     *   リクエストURLからベースURLを引いた文字列（PathInfo）を返す
+     *   ただしURLパラメーターがある時はリクエストURLからURLパラメーターを除いたものを使用する
+     *   ※PathInfoとは？
+     *     ベースURLより後ろの値になる、ただしURLパラメーターは含まない
+     *     例：「http://hostname.com/foo/bar/list(index.phpは"/foo/bar"以下) → 「/list」
+     *         「http://hostname.com/index.php/list?foo=bar」                → 「/list」
+     *         「http://hostname.com/」                                      → 「/」
+     * 
+     * @return string 
+     */
     public function getPathInfo()
     {
 
+        // ベースURLとリクエストURLを取得する
         $baseUrl    = $this->getBaseUrl();
         $requestUri = $this->getRequestUri();
 
+        // リクエストされたURLに「？」があった時
+        // ※strposで特定の単語が見つからなかった時は「False」を返す
         if ( false != ($pos = strpos($requestUri, '?')) ) {
 
+            // リクエストURLから「？」の直前までのURLを取得
             $requestUri = substr($requestUri, 0, $pos);
 
         }
 
+        // リクエストURLからベースURLを引いた文字列を取得
         $pathInfo = (string)substr($requestUri, strlen($baseUrl));
 
         return $pathInfo;
